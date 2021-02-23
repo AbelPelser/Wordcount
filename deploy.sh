70dad32c1aef22eee03ec537654b8be251f73d7d
AbelPelser <abelpelser@gmail.com>
2021-02-23 11:45:42 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,2 @@
+# Assuming Payara micro jar is in cwd, and the location of the jar is as below.
 java -jar payara-micro-5.2021.1.jar --deploy ~/IdeaProjects/wordcount-ee-ordina/target/wordcount-1.0.war
